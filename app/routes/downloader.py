14c41fa7a5235b298acc11969842fe7e6b9d3ca4
f0rk-it <mzscripterx5@gmail.com>
2025-06-20 05:45:00 +0100

    Tested on mobile.

--- app/routes/downloader.py
+++ app/routes/downloader.py
@@ -16,6 +16,7 @@ async def download_video(body: DownloadRequest):
     try:
         output_path = download_and_merge_youtube_video(url)
         filename = os.path.basename(output_path)
+        # return FileResponse(output_path, media_type='video/mp4', filename=output_path.split('/')[-1])
         return {
             'message': 'Download complete',
             'filename': filename,
