7f8a4cd8aeabc4dd2f1f09181fd28ffb9b1b8c88
f0rk-it <mzscripterx5@gmail.com>
2025-06-21 13:46:05 +0100

    Fixed the issues with the temp files.

--- app/routes/downloader.py
+++ app/routes/downloader.py
@@ -1,14 +1,22 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, BackgroundTasks
 from fastapi.responses import FileResponse
 from app.services.yt import download_and_merge_youtube_video
 from app.models.schemas import DownloadRequest
 import os
 from yt_dlp import YoutubeDL
+import time
 
 router = APIRouter()
 
+def delete_file_later(path: str):
+    time.sleep(10)
+    if os.path.exists(path):
+        print(f'Removing temporary file: {path}')
+        os.remove(path)
+        print(f'Temporary file {path} removed successfully.')
+
 @router.post('/download')
-async def download_video(body: DownloadRequest):
+async def download_video(body: DownloadRequest, background_tasks: BackgroundTasks):
     url = body.url.strip()
     
     if not url:
@@ -18,12 +26,13 @@ async def download_video(body: DownloadRequest):
         output_path = download_and_merge_youtube_video(url)
         filename = os.path.basename(output_path)
         print(f'Downloaded video: {filename}')
-        return FileResponse(path=output_path, media_type='video/mp4', filename=filename)
-        # return {
-        #     'message': 'Download complete',
-        #     'filename': filename,
-        #     'path': f'files/{filename}'
-        # }
+        
+        # Schedule cleanup after response is sent
+        background_tasks.add_task(delete_file_later, output_path)
+        
+        # Serve the file as a response
+        return FileResponse(path=output_path, media_type='video/mp4', filename=filename, background=background_tasks)
+    
     except Exception as e:
         return {'error': str(e)}
     
